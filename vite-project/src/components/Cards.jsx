a93229db72bcfa0285e8c5979db1f46da61e1ccd
unknown <nhj25252@gmail.com>
2023-07-28 11:25:31 +0900

    Add Cards.css

--- vite-project/src/components/Cards.jsx
+++ vite-project/src/components/Cards.jsx
@@ -1,10 +1,15 @@
 import cardsData from '../data/cards';
+import '../styles/Cards.css';
 
 function Cards() {
   // render cardsData array => to items
   // div is each card
 
-  const renderedCards = cardsData.map((card) => <div key={card.id}>{card.name}</div>);
+  const renderedCards = cardsData.map((card) => (
+    <div className="card" key={card.id}>
+      {card.name}
+    </div>
+  ));
 
   return renderedCards;
 }
