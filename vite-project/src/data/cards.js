fc6bbb4f7aa519af61e153d484172de8e4afc07f
unknown <nhj25252@gmail.com>
2023-07-28 11:16:23 +0900

    Add uuid to each card object

--- vite-project/src/data/cards.js
+++ vite-project/src/data/cards.js
@@ -1,12 +1,19 @@
-//import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
+
+// each card data must have an id
 const cardsData = [
   {
     characters: [
-      { name: 'Princess Bubblegum', src: 'princessBubblegum.png' },
-      { name: 'Jake', src: 'jake.png' },
-      { name: 'Fin', src: 'fin.png' },
-      { name: 'Cupcake', src: 'cupcake.png' },
-      { name: 'Ice King', src: 'iceKing.png' },
+      { name: 'Princess Bubblegum', src: 'princessBubblegum.png', id: uuidv4() },
+      { name: 'Jake', src: 'jake.png', id: uuidv4() },
+      { name: 'Fin', src: 'fin.png', id: uuidv4() },
+      { name: 'Cupcake', src: 'cupcake.png', id: uuidv4() },
+      { name: 'Ice King', src: 'iceKing.png', id: uuidv4() },
+      { name: 'Marceline', src: 'marceline.png', id: uuidv4() },
+      { name: 'Lumpy Space Princess', src: 'lumpySpacePrincess.png', id: uuidv4() },
+      { name: 'Bmo', src: 'bmo.png', id: uuidv4() },
+      { name: 'Dancing Fin', src: 'dancingFin.png', id: uuidv4() },
+      { name: 'Dancing Princess Bubblegum', src: 'dancingBubblegum.png', id: uuidv4() },
     ],
   },
 ];
