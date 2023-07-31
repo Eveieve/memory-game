89a11552b7d0340549e23369f74f3853b5f22123
unknown <nhj25252@gmail.com>
2023-07-31 14:04:36 +0900

    [feat] Set best score

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,6 +3,7 @@ import Cards from './components/Cards.jsx';
 import Scoreboard from './components/Scoreboard.jsx';
 import { useState } from 'react';
 import cardsData from './data/cards.js';
+import shuffleArray from './utils/shuffle-cards.js';
 
 function App() {
   const [cards, setCards] = useState(cardsData);
@@ -10,9 +11,9 @@ function App() {
   console.log(setCards);
 
   const [currentScore, setScore] = useState(0);
-
+  const [bestScore, setBestScore] = useState(0);
   // handles SingleCard click
-  function handleClick(e) {
+  function handleClick(e, id) {
     //when the card is clicked
     // AND when it's never been clicked
     // increment the currentScore by 1.
@@ -21,27 +22,20 @@ function App() {
       setScore(currentScore + 1);
       console.log(currentScore);
       e.target.dataset.isclicked = 'true';
+      // mix cards after each click
+      shuffleArray(cardsData);
+    } else {
+      if (currentScore > bestScore) {
+        setBestScore(currentScore);
+      }
+      setScore(0);
     }
-
-    // mix cards after each click
-    shuffleArray(cardsData);
-  }
-
-  // returns randomized array
-  function shuffleArray(array) {
-    let newArr = [...array];
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-
-    return newArr;
   }
 
   return (
     <header className="header">
       <h1>Adventure Time Memory Game</h1>
-      <Scoreboard currentScore={currentScore} setScore={setScore} />
+      <Scoreboard currentScore={currentScore} setScore={setScore} bestScore={bestScore} />
       <Cards currentScore={currentScore} setScore={setScore} handleClick={handleClick} />
     </header>
   );
